192ef49e238147cba5d82176998b406eb03981a5
neilroy1998 <neilroy1998@gmail.com>
2019-04-02 14:17:38 +0530

    Progress Bar

--- app/src/main/java/com/example/perceptive/searchActivity.java
+++ app/src/main/java/com/example/perceptive/searchActivity.java
@@ -13,8 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -23,6 +25,9 @@ public class searchActivity extends AppCompatActivity {
 
     Button search_popup;
     public static ListView search_lv;
+    public int type = -1;
+
+    static ProgressBar pb;
 
     static Context context;
 
@@ -38,8 +43,10 @@ public class searchActivity extends AppCompatActivity {
         context = searchActivity.this;
 
         search_lv = findViewById(R.id.search_lv);
-
         search_lv.setVisibility(View.GONE);
+
+        pb = findViewById(R.id.search_progressBar);
+        pb.setVisibility(View.GONE);
 
         search_topic = findViewById(R.id.search_et_topic_et);
 
@@ -57,14 +64,17 @@ public class searchActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.type_movies:
                                 search_popup.setText("MOVIES");
+                                type = 0;
                                 search_popup.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_movie_black_24dp, 0,0,0);
                                 return true;
                             case R.id.type_songs:
                                 search_popup.setText("SONGS");
+                                type = 1;
                                 search_popup.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_music_note_black_24dp, 0,0,0);
                                 return true;
                             case R.id.type_books:
                                 search_popup.setText("BOOKS");
+                                type = 2;
                                 search_popup.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_book_black_24dp, 0,0,0);
                                 return true;
                             default:
@@ -82,7 +92,21 @@ public class searchActivity extends AppCompatActivity {
 
     public void search_bclk(View v) {
 
-        fetch_data fd = new fetch_data(search_topic.getText().toString());
+        fetch_data fd = null;
+        pb.setVisibility(View.VISIBLE);
+        if (type == -1) {
+            Toast.makeText(context, "Set Type", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (type == 1) {
+            fd = new fetch_data(search_topic.getText().toString());
+        }
+        if (type == 0) {
+            return;
+        }
+        if (type == 2) {
+            return;
+        }
         fd.execute();
 
     }
@@ -90,6 +114,7 @@ public class searchActivity extends AppCompatActivity {
     public static void update_list(String arr[]) {
         ArrayAdapter<String> searchAA = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, arr);
         search_lv.setAdapter(searchAA);
+        pb.setVisibility(View.GONE);
         search_lv.setVisibility(View.VISIBLE);
     }
 
